510e71495eb21eed31f2e1454e653a29aacd34ae
Lee Joo Hyung <wngud985@gmail.com>
2024-12-30 23:28:54 +0900

    fix(calendar): Fix date formatting

--- daily-vibes-frontend/src/app/calendar/page.tsx
+++ daily-vibes-frontend/src/app/calendar/page.tsx
@@ -86,7 +86,13 @@ export default function Home() {
     ({ date }: { date: Date }) => {
       if (dailyData?.length) {
         for (let i = 0; i < dailyData.length; i++) {
-          const calendarDate = date.toLocaleDateString();
+          const year = date.getFullYear();
+          const month = String(date.getMonth() + 1).padStart(2, "0");
+          const day = String(date.getDate()).padStart(2, "0");
+
+          const formattedDate = `${month}/${day}/${year}`;
+
+          const calendarDate = formattedDate;
           const contentDate = dailyData[i].contentDate;
 
           if (calendarDate === contentDate) {
